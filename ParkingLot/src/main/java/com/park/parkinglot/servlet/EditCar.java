e2f3665b838e09809617be753538a2ed73681b58
StirbetStefan <stefan.stirbet@ulbsibiu.ro>
2022-01-05 13:46:29 +0200

    CRUD and Validation

--- ParkingLot/src/main/java/com/park/parkinglot/servlet/EditCar.java
+++ ParkingLot/src/main/java/com/park/parkinglot/servlet/EditCar.java
@@ -5,11 +5,12 @@
 package com.park.parkinglot.servlet;
 
 import com.park.parkinglot.common.CarDetails;
+import com.park.parkinglot.common.UserDetails;
 import com.park.parkinglot.ejb.CarBean;
+import com.park.parkinglot.ejb.UserBean;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
-import java.util.ArrayList;
 import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -21,8 +22,8 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Stefan
  */
-@WebServlet(name = "Cars", urlPatterns = {"/Cars"})
-public class Cars extends HttpServlet {
+@WebServlet(name = "EditCar", urlPatterns = {"/EditCar"})
+public class EditCar extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -33,10 +34,12 @@ public class Cars extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    
-    @Inject 
-    private CarBean carBean;
-    
+    @Inject
+    UserBean userBean;
+
+    @Inject
+    CarBean carBean;
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -45,10 +48,10 @@ public class Cars extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet Cars</title>");            
+            out.println("<title>Servlet EditCar</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet Cars at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet EditCar at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
@@ -66,13 +69,15 @@ public class Cars extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        //processRequest(request, response);
-        request.setAttribute("ativePage", "Cars");
-        request.setAttribute("numberOfFreeParkingSpots",10);
 
-        List<CarDetails> cars= carBean.getAllCars();
-        request.setAttribute("cars",cars);
-        request.getRequestDispatcher("/WEB-INF/pages/cars.jsp").forward(request, response);
+        List<UserDetails> users = userBean.getAllUsers();
+        request.setAttribute("users", users);
+
+        int carId = Integer.parseInt(request.getParameter("id"));
+        CarDetails car = carBean.findById(carId);
+        request.setAttribute("car", car);
+
+        request.getRequestDispatcher("/WEB-INF/pages/editCar.jsp").forward(request, response);
     }
 
     /**
@@ -86,16 +91,15 @@ public class Cars extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String[] carIdsAsString = request.getParameterValues("car_ids");
-        if(carIdsAsString != null){
-            List<Integer> carIds = new ArrayList<>();
-            for(String carIdAsString : carIdsAsString){
-                carIds.add(Integer.parseInt(carIdAsString));
-            }
-            carBean.deleteCarsById(carIds);
-        }
 
-        response.sendRedirect(request.getContextPath()+ "/Cars");
+        String licensePlate = request.getParameter("license_plate");
+        String parkingSpot = request.getParameter("parking_spot");
+        int userId = Integer.parseInt(request.getParameter("owner_id"));
+        int carId = Integer.parseInt(request.getParameter("car_id"));
+
+        carBean.updateCar(carId, licensePlate, parkingSpot, userId);
+
+        response.sendRedirect(request.getContextPath() + "/Cars");
     }
 
     /**
--- ParkingLot/src/main/webapp/WEB-INF/pages/cars.jsp
+++ ParkingLot/src/main/webapp/WEB-INF/pages/cars.jsp
@@ -10,21 +10,30 @@
 
 <t:pageTemplate pageTitle="Cars">
     <h1>Cars</h1>
-    <c:forEach var="car" items="${cars}" varStatus="status">
+    <form method="POST" action="${pageContext.request.contextPath}/Cars">
+        <a class="btn btn-primary btn-lg" href="${pageContext.request.contextPath}/AddCar" role="button">Add Car</a>
+        <button class="btn btn-danger" type="submit">Delete Cars</button>
 
-        <div class="row">
-            <div class="col-md-4">
-                ${car.licensePlate}
-            </div>
-            <div class="col-md-4">
-                ${car.parkingSpot}
-            </div>
-            <div class="col-md-4">
-                ${car.username}
-            </div>
-        </div>
+        <c:forEach var="car" items="${cars}" varStatus="status">
 
-    </c:forEach>
-
+            <div class="row">
+                <div class="col-md">
+                    <input type="checkbox" name="car_ids" value="${car.id}" />
+                </div>
+                <div class="col-md-3">
+                    ${car.licensePlate}
+                </div>
+                <div class="col-md-3">
+                    ${car.parkingSpot}
+                </div>
+                <div class="col-md-3">
+                    ${car.username}
+                </div>
+                <div class="col-md-2">
+                    <a class="btn btn-secondary" href="${pageContext.request.contextPath}/EditCar?id=${car.id}" role="button">Edit Car</a>
+                </div>
+            </div>
+        </c:forEach>
+    </form>
     <h5>Free parking spots: ${numberOfFreeParkingSpots}</h5>
 </t:pageTemplate>
